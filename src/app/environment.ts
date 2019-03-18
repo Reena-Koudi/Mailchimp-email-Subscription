4ec51e1390afe9a5019caedb5647c250035b46c2
Reena Koudi <reena.koudi@holition.com>
2019-03-18 15:09:28 +0000

    small tweak

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 mailchimp: {
 apiUrl: 'https://wojooh.us19.list-manage.com/subscribe/post?u=4a0a6af22e69eb2da0f8f249c&amp;id=cd694b89be',
-hiddenInput: 'b_4a0a6af22e69eb2da0f8f249c_cd694b89be'
+hiddenInputName: 'b_4a0a6af22e69eb2da0f8f249c_cd694b89be'
 }
 };
